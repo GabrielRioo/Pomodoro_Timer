6a942b907b7486a198fb570200c937d0d8f6af1f
GabrielRioo <gabrielpaivario@gmail.com>
2023-06-02 20:23:25 -0300

    Creating the history page with a table and status

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,7 +1,50 @@
+import { HistoryContainer, HistoryList, Status } from './styles'
+
 export function History() {
   return (
-    <>
-      <h1>Home</h1>
-    </>
+    <HistoryContainer>
+      <h1>Meu histórico</h1>
+
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Inicio</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 Minutos</td>
+              <td>há 2 meses</td>
+              <td>
+                <Status statusColor="green"> Concluido </Status>
+              </td>
+            </tr>
+
+            <tr>
+              <td>Tarefa</td>
+              <td>20 Minutos</td>
+              <td>há 2 meses</td>
+              <td>
+                <Status statusColor="yellow"> Em andamento </Status>
+              </td>
+            </tr>
+
+            <tr>
+              <td>Tarefa</td>
+              <td>20 Minutos</td>
+              <td>há 2 meses</td>
+              <td>
+                <Status statusColor="red"> Interrompido </Status>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
   )
 }
